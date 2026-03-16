ccf391eda7780515d2b510e45c06fe7a1f096945
Hemanth Krishna <hemanthkrishna.ab@anorasolutions.com>
2026-03-16 15:11:19 +0530

    added functions

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,5 +1,19 @@
 #include "MyClass.h"
+#include <iostream>
 
 void MyClass::printAge(){
-
+    std::cout<<age;
 }
+
+void MyClass::printName(){
+    std::cout<<name<<std::endl;
+}
+
+void MyClass::setAge(int a){
+    age = a;
+}
+
+void MyClass::setName(const std::string s){
+    name = s;
+}
+
